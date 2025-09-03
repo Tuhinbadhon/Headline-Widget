3f1e82c4c415a3e5b65efaa2d9adfaaec9b2ee5f
Tuhinbadhon <tuhinbadhon@gmail.com>
2025-09-04 00:11:28 +0600

    updaed

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,1 @@
-// Export all types from headline types
 export * from "./headline";
-
-// You can add more type exports here as the project grows
-// export * from './ui';
-// export * from './api';
-// export * from './utils';
